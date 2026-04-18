2be4ab414d4972ded514078734d79ee1352a7aa8
Hasini Bandara <dmhnbandara@gmail.com>
2026-04-18 18:26:08 +0530

    payment

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://smart-store-frontend.vercel.app',
-    ],
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:5173',
+    'https://smart-store-frontend.vercel.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe());
 
